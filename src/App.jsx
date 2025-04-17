a0888e080f6109ed90d32a14ab88317fe9a331ff
zakjpeg <elassaadzak@gmail.com>
2025-04-17 14:01:29 -0700

    build experiment

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <HashRouter>
       <div className="header">
         <div className="spacer-left"></div>
-        <Link to="/"><img src="./public/cartsyLogoName.png" alt="" /></Link>
+        <Link to="/"><img src="./images/cartsyLogoName.png" alt="" /></Link>
         <nav className="nav__top">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
